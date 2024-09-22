df5452cde94e218cdc173a1f8131189853fb77da
vishwesh5544 <vishweshshukla20@gmail.com>
2024-09-22 20:22:06 +0530

    added example env and mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoleModule } from './role/role.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [RoleModule],
+  imports: [RoleModule,
+    MongooseModule.forRoot(process.env.MONGO_URI)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
